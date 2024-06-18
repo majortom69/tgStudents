c5bb72908ce26a1a5d29efa7531c2468cf0aaf47
Bomb <unknownsoldier2005gmail.com>
2024-06-18 21:01:43 +0300

    i love black people

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -6,7 +6,7 @@ export const command = {
     execute: (bot: Telegraf, ctx: Context) => {
         // Когда мы нажимаеv на кнопку оно оправляет сигнал в '.src/events/button_click.ts'
         const button: InlineKeyboardButton = { text: 'Button', callback_data: 'button_click' };
-        ctx.reply('fuck you',  {
+        ctx.reply('I hate niggers',  {
             reply_markup: {
                 inline_keyboard: [[button]]
             }
