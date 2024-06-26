4d72535e7ddb41177fa5a5a3f05ed51f6931afd4
1945Bomb <unknownsoldier2005@gmail.com>
2024-06-26 19:53:08 +0300

    help

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,10 +1,11 @@
-import { Context, Telegraf,  Markup  } from 'telegraf';
+import { Context, Telegraf } from 'telegraf';
 
 export const command = {
     name: 'help',
     execute: async (bot: Telegraf, ctx: Context) => {
         const commands = await bot.telegram.getMyCommands();
         const helpMessage = commands
+            .filter(command => command.command !== 'help')
             .map(command => `/${command.command} - ${command.description}`)
             .join('\n');
         ctx.reply(helpMessage || 'No commands available.');
