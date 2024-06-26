2a1362673763aa6c611981f3b9d57352bb6c82a6
1945Bomb <unknownsoldier2005@gmail.com>
2024-06-26 19:15:05 +0300

    huy

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,2 +1,12 @@
 // Сделать команду для приветсвия
 // Она выводится автоматически, когда пользователь впервые запускает бота
+
+import { Context, Telegraf,  Markup  } from 'telegraf';
+import { InlineKeyboardButton } from 'telegraf/typings/core/types/typegram';
+
+export const command = {
+    name: 'start',
+    execute: (bot: Telegraf, ctx: Context) => {
+        ctx.reply('I hate niggers');
+    }
+};
